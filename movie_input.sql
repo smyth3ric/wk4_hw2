0600cf2c18d4eceae60314ef3e14a78c8d433127
eric smith <smyth.3ric@gmail.com>
2022-10-12 18:33:19 -0400

    second commit

---
+++ movie_input.sql
@@ -0,0 +1,61 @@
+INSERT INTO customer (
+    first_name,
+    last_name,
+    phone, 
+    email
+    )
+VALUES (
+    'Eric', 
+    'Smith',
+    '757-467-7351',
+    'eric@email.com'
+);
+
+INSERT INTO product (
+    product_name,
+    price,
+    quantity)
+
+VALUES (
+    'popcorn',
+    3.50,
+    100
+);
+
+
+INSERT INTO concession (
+    payment_date
+)
+    
+VALUES (
+    '10/31/2021'
+);
+
+   
+
+INSERT INTO ticket (
+    price,
+    payment_date)
+    
+VALUES (
+    17.00,
+    "10/31/2021"
+);
+
+INSERT INTO movie (
+    title,
+    genre,
+    release_date,
+    actor,
+    rating,
+    film_studio)
+    
+VALUES (
+    'The Fly',
+    'Horror, Sci-fi',
+    '1986',
+    'Jeff Goldblum',
+    'R',
+    '20th Century Fox'
+
+);
